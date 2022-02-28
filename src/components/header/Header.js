a17cbe7eed74a73ff004aa578e90d2117c5b8ef9
Alif-07 <mokaddis.alif@gmail.com>
2022-03-01 00:46:39 +0600

    second commit

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,14 +3,32 @@ import CTA from './CTA';
 import './Header.css';
 import ME from '../../assets/me.png';
 import HeaderSocial from './HeaderSocial';
+import Typewriter from 'typewriter-effect';
 
 const Header = () => {
 	return (
 		<header>
 			<div className="container header__container">
-				<h5>Hello I'm</h5>
+				{/* <h5>Hello I'm</h5>
 				<h1>Mokaddis Borhan ALif</h1>
-				<h5 className="text-light">MERN Stack Developer</h5>
+				<h5 className="text-light">MERN Stack Developer</h5> */}
+
+				<span style={{ fontSize: '30px' }}>
+					<Typewriter
+						onInit={(typewriter) => {
+							typewriter
+								.typeString(`Hello I'm`)
+								.pauseFor(2000)
+								.deleteAll()
+								.typeString('Mokaddis Borhan ALIF')
+								.pauseFor(1000)
+								.deleteAll()
+								.typeString('MERN Stack Developer')
+								.start();
+						}}
+					/>
+				</span>
+
 				<CTA />
 				<HeaderSocial />
 
