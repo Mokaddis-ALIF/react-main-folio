185a35247dc1da0758d8ded209e9a9c6688bc322
Alif-07 <mokaddis.alif@gmail.com>
2022-03-02 22:23:02 +0600

    header btn design

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import CV from '../../assets/CV.pdf';
+import { TiDownloadOutline } from 'react-icons/ti';
+import { TiMessages } from 'react-icons/ti';
 
 const CTA = () => {
 	return (
 		<div className="cta">
-			<a className="btn" href={CV} download>
-				Download CV
+			<a className="btn" id="cv" href={CV} download>
+				Download CV <TiDownloadOutline />
 			</a>
-			<a className="btn-primary" href="#contact">
-				Let's Talk
+			<a className="btn-primary" id="cv" href="#contact">
+				Let's Talk <TiMessages />
 			</a>
 		</div>
 	);
