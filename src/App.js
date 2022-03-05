5e5d2c28c752bd3c90458488aee8340f0da82bba
Alif-07 <mokaddis.alif@gmail.com>
2022-03-05 17:42:31 +0600

    spinner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import About from './components/about/About';
 import Experience from './components/experience/Experience';
 import Header from './components/header/Header';
@@ -7,18 +7,41 @@ import Portfolio from './components/portfolio/Portfolio';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import Services from './components/services/Services';
+import RingLoader from 'react-spinners/RingLoader';
 
 const App = () => {
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+		}, 1000);
+	}, []);
+
 	return (
 		<>
-			<Header />
-			<Nav />
-			<About />
-			<Experience />
-			<Services />
-			<Portfolio />
-			<Contact />
-			<Footer />
+			{loading ? (
+				<div className="spinner">
+					<RingLoader
+						color={'#4db5ff'}
+						loading={loading}
+						size={500}
+						speedMultiplier={0.8}
+					/>
+				</div>
+			) : (
+				<>
+					<Header />
+					<Nav />
+					<About />
+					<Experience />
+					<Services />
+					<Portfolio />
+					<Contact />
+					<Footer />
+				</>
+			)}
 		</>
 	);
 };
